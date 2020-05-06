e5eb10ae7ffe69ce984b8b7de44e43931ee902a8
bigonia <1533973672@qq.com>
2020-05-06 22:15:10 +0800

    完善utils.config类的相关代码

--- src/main/java/com/infoc/controller/DataSource.java
+++ src/main/java/com/infoc/controller/DataSource.java
@@ -1,6 +1,8 @@
 package com.infoc.controller;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,15 +13,23 @@ import java.nio.charset.Charset;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+
 import net.sf.json.JSONObject;
 
 public class DataSource {
 //	private final 
-	private static String jcwurl = "http://www.cninfo.com.cn/new/index/getAnnouces?type=sh";
-	private static String sjsurl = "http://www.sse.com.cn/disclosure/listedinfo/bulletin/s_docdatesort_desc_2019openpdf.htm";
+//	private  String jcwurl = "http://www.cninfo.com.cn/new/index/getAnnouces?type=sh";
+//	private  String sjsurl = "http://www.sse.com.cn/disclosure/listedinfo/bulletin/s_docdatesort_desc_2019openpdf.htm";
 	
-	//获取json数据
-	public static JSONObject JsonData(String url) throws MalformedURLException, IOException {
+	/**
+	 * 	通过流获取json的初始数据
+	 * 	？使用Jsoup直接获得json
+	 * @param url
+	 * @return JSONObject
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	public JSONObject getJsonData(String url) throws MalformedURLException, IOException {
 		//读取网站数据
 		InputStream is = new URL(url).openStream();
 		BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));  
@@ -31,18 +41,61 @@ public class DataSource {
         }  
         //得到json
         String jsonText = sb.toString();  
-		return JSONObject.fromObject(jsonText);
+		
+        return JSONObject.fromObject(jsonText);
+        
+//		return JSONObject.fromObject(Jsoup.connect(url).get());
 		
 	}
 	
-	//获取html数据
-	public static Document HtmlData(String url) throws IOException {
-		
-		//http://www.sse.com.cn/disclosure/listedinfo/bulletin/s_docdatesort_desc_2019openpdf.htm
-//		Document document = Jsoup.connect(url).get();
+	/**
+	 * 	使用Jsoup直接获得Document
+	 * @param url
+	 * @return	Document
+	 * @throws IOException
+	 */
+	public Document getHtmlData(String url) throws IOException {
 		
 		return Jsoup.connect(url).get();
 		
 	}
+	
+	/**
+	 * 	统一从两个网站抓取数据的方法,抓取结果转换为String返回;
+	 * 	tip：1使用String实例化document
+	 * 		 2使用String实例化JSONObject
+	 * @param url
+	 * @return	String
+	 * @throws IOException
+	 */
+	public String getStringData(String url) throws IOException {
+		
+//		String JCWurl = "http://www.cninfo.com.cn/new/index/getAnnouces?type=sh";
+//		String SJSurl = "http://www.sse.com.cn/disclosure/listedinfo/bulletin/s_docdatesort_desc_2019openpdf.htm";
+		//字符输入流
+		InputStream is = null;
+		//转换流
+		InputStreamReader isr = null;
+		//缓冲字符输入流
+		BufferedReader br = null;
+		
+		is = new URL(url).openStream();
+		isr = new InputStreamReader(is);
+		br = new BufferedReader(isr);
+		
+		StringBuffer sb = new StringBuffer();
+		int cp;
+		while((cp = br.read()) != -1) {
+			sb.append((char)cp);
+		}
+		//测试SJS
+//		String str = sb.toString();
+//		System.out.println(str);
+//		new TestSJSh().SJSTest(str);
+		
+		return sb.toString();
+		
+	}
+	
 	
 }
--- src/main/java/com/infoc/utils/Config.java
+++ src/main/java/com/infoc/utils/Config.java
@@ -1,5 +1,46 @@
 package com.infoc.utils;
 
-public class Config {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+/**
+ * 	配置类：用于配置项目的各项属性；利用propoties读取配置信息；
+ * @author Administrator
+ *
+ */
+public class Config {
+	
+	//巨潮网数据的url
+	public  static String JCW_URL = "http://www.cninfo.com.cn/new/index/getAnnouces?type=sh";
+	//上交所数据的url
+	public  static String SJS_URL = "http://www.sse.com.cn/disclosure/listedinfo/bulletin/s_docdatesort_desc_2019openpdf.htm";
+	//propoties
+	private static Properties properties = new Properties();
+	
+	//静态代码块；类加载时读取各项配置信息并相应赋值
+	static {
+		//目前需要配置的内容：两个网站url、发送的邮件地址、...
+		
+		//读取propoties /infoComparison/src/main/resources/config.properties
+		InputStream in = ClassLoader
+                .getSystemResourceAsStream("src/main/resources/config.properties");
+		try {
+			properties.load(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void listPropoties() {
+		for (String key : properties.stringPropertyNames()) {
+			System.out.println(key + "=" + properties.getProperty(key));
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Config().listPropoties();
+	}
+	
 }
